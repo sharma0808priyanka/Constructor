859a26b485f855bca42373703b0e647722352719
Priyanka Sharma <71631595+sharma0808priyanka@users.noreply.github.com>
2021-04-02 20:11:22 +0530

    Add files via upload
    I write some easy examples of constructor so that is helpful for beginers.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace prct_constructor
+{
+    class Person
+    {
+        private string name;
+        private  Person()
+        {
+            this.name = "abhay";
+            
+        }
+         public Person(string myname)
+        {
+            this.name = myname;
+        }
+              
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pr = new Person("priya");  
+        }
+    }
+}
